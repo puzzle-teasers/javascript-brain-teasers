789f2f67bc9a8f5ac5988ba92d6179d8cd7089ab
arisromil <arisromil@yahoo.com>
2025-08-01 12:28:16 -0600

    Create BitWise.js

---
+++ BitWise.js
@@ -0,0 +1,8 @@
+// 7 The tilde (~) symbol in JavaScript is known as the bitwise NOT
+//operator. It takes the binary representation of a number and flips all the
+//bits, resulting in a new number. Now, the thing is, bitwise operators in
+//JavaScript work with integers, not floating-point numbers.
+
+const x = ~~(7.9);
+
+console.log(x);
