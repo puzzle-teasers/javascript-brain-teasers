e4526f0e90ce5fb0a676dd33e2fd4cf779746dae
arisromil <arisromil@yahoo.com>
2025-08-26 09:38:42 -0600

    Create NoNumber.js

---
+++ NoNumber.js
@@ -0,0 +1,10 @@
+// NaN
+// declares a constant variable without assigning any value to it. So, the variable holds a default value
+// of undefined. When trying to add undefined to the value of price, the
+// operation yields NaN (Not-a-Number).
+let price = 10;
+let tax;
+
+const sum = price + tax;
+
+console.log(sum);
